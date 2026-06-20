98c654b63a8da44d52c7f2367214382a5e3ea4fa
Natdanai Khemtong <natdanai.khemt@gmail.com>
2026-06-21 00:36:30 +0700

    Make cacheTTL configurable via environment variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
   apiBaseUrl: import.meta.env.VITE_API_ENDPOINT + "/api",
-  cacheTTL: 24 * 60 * 60 * 1000, // 24 hours
+  cacheTTL: (import.meta.env.VITE_CACHE_TTL as number) || 24 * 60 * 60 * 1000, // 24 hours
 } as const;
